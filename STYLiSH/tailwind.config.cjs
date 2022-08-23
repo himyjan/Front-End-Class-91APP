52b8355122df30970acf7b08893fb5592992dd97
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-23 13:24:13 +0800

    day22 w2p3

--- STYLiSH/tailwind.config.cjs
+++ STYLiSH/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "0px",
+        // => @media (min-width: 640px) { ... }
+
+        mobile360: "360px",
+        // => @media (min-width: 640px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 };

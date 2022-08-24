7ba9a6431dc1c4cf47215edb72327311c31e082d
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-24 20:14:05 +0800

    day23 update project

---
+++ STYLiSH/src/Carousel.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import {} from "./StyledComponents/Carousel.style";
+
+function Carousel() {
+  return <div className="Carousel"></div>;
+}
+
+export default Carousel;

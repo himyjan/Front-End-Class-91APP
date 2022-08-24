41f2a9f9be78e1370b12f376272e0b7db6399b47
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-24 17:54:19 +0800

    day23 update project vite ts

---
+++ STYLiSH-vitejs/src/Thankyou.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import {} from "./StyledComponents/Thankyou.style";
+
+function Thankyou() {
+  return <div className="Thankyou"></div>;
+}
+
+export default Thankyou;
--- STYLiSH-vitejs/src/index.tsx
+++ STYLiSH-vitejs/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import styled from "styled-components";
+import GlobalStyle from "./StyledComponents/index.style";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Carousel from "./Carousel";
+import Product from "./Product";
+import Checkout from "./Checkout";
+import Thankyou from "./Thankyou";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <Home />
+    <GlobalStyle />
+  </React.StrictMode>
+);

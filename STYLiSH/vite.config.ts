897ddf7762200c5641cb5fad503a0628e3082208
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-25 08:59:21 +0800

    day24 enable to start creating style component  OK  vite

--- STYLiSH/vite.config.ts
+++ STYLiSH/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,12 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    outDir: "./build",
+    rollupOptions: {
+      input: {
+        main: resolve("./", "index.html"),
+      },
+    },
+  },
 });

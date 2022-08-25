9922c457d8847c6e7a9d84a2495d05a46902153a
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-26 07:24:06 +0800

    day24 update project

--- STYLiSH/src/StyledComponents/index.style.tsx
+++ STYLiSH/src/StyledComponents/index.style.tsx
@@ -3,7 +3,7 @@ import mediaQuery from "./mediaQuery";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-* {
+*,body {
   margin: 0;
   @import url('https://fonts.googleapis.com/css?family=Noto Sans TC');
   font-family: 'Noto Sans TC';

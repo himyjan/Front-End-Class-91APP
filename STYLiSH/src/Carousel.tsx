1c7e843e76832c649f42b6878857c07647827231
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-28 13:20:17 +0800

    day25 w2p4 home page slider in react and sc 1.click ot to chage 2.mouse hover stop

--- STYLiSH/src/Carousel.tsx
+++ STYLiSH/src/Carousel.tsx
@@ -1,24 +1,89 @@
 import Props from "./types/styleComponentsType";
+import { ApiData, getSliderData } from "./lib/fetchAPI";
+import { useInterval } from "usehooks-ts";
+import { useState, useEffect } from "react";
+import { Slides, Dot } from "./styledComponents/Carousel.style";
 
 const Carousel = ({ className }: Props) => {
+  const [sliderIndex, setSliderIndex] = useState(0);
+  const [pause, setPause] = useState(false);
+  const sliderData = getSliderData();
+
+  useEffect(() => {
+    const sliderDataCopy: ApiData[] = [...sliderData];
+    sliderDataCopy.forEach((slider) => {
+      const img = new Image();
+      img.src = (slider as any).picture;
+    });
+  }, [sliderData]);
+
+  useInterval(
+    () => {
+      sliderIndex == sliderData.length - 1
+        ? setSliderIndex(0)
+        : setSliderIndex(sliderIndex + 1);
+    },
+    pause ? null : 5000
+  );
+
   return (
     <div className={className}>
-      <div className="slides">
-        <div className="slides-box">
+      <Slides
+        className="slides"
+        picture={
+          sliderData.length > 0
+            ? (sliderData[sliderIndex] as any).picture
+            : null
+        }
+      >
+        <div
+          className="slides-box"
+          onMouseEnter={() => setPause(true)}
+          onMouseLeave={() => setPause(false)}
+        >
           <div className="slides-slidesTextMain">
             <div className="slides-text-main">
-              於是
-              <br />
-              我也想要給你
-              <br />
-              一個那麼美好的自己。
-              <br />
+              {/* 於是 */}
+              {sliderData.length > 0
+                ? (sliderData[sliderIndex] as any).story
+                    .split("。")[0]
+                    .replaceAll("\r\n", "\n")
+                : null}
+              {/* <br /> */}
+              {/* 我也想要給你 */}
+              {/* <br /> */}
+              {/* 一個那麼美好的自己。 */}
+              {/* <br /> */}
             </div>
-            <div className="slides-text-describe">不朽《與自己和好如初》</div>
+            <div className="slides-text-describe">
+              {/* 不朽《與自己和好如初》 */}
+              {sliderData.length > 0
+                ? (sliderData[sliderIndex] as any).story
+                    .split("。")[1]
+                    .replaceAll("<br>", "")
+                : null}
+              {/* {Object.keys(campaigns).length > 0
+                ? (campaigns[sliderIndex] as any).story
+                    .split("。")[1]
+                    .replaceAll("<br>", "")
+                : null} */}
+            </div>
           </div>
         </div>
         <div className="main-page-dot-box">
-          <div
+          {sliderData.map((item, index) => {
+            return (
+              <Dot
+                data-index={index}
+                id={(item as any).id}
+                data-product_id={parseInt((item as any).product_id)}
+                color= {sliderIndex==index?"#8b572a":"#ffffff40"}
+                className="dot"
+                onClick={() => setSliderIndex(index)}
+              ></Dot>
+            );
+          })}
+          {/* <div
             data-index=""
             id=""
             data-product_id=""
@@ -52,9 +117,9 @@ const Carousel = ({ className }: Props) => {
             data-product_id=""
             className="dot
         "
-          ></div>
+          ></div> */}
         </div>
-      </div>
+      </Slides>
     </div>
   );
 };
--- STYLiSH/src/index.tsx
+++ STYLiSH/src/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import GlobalStyle from "./styledComponents/index.style";
 import Header from "./styledComponents/Header.style";
@@ -7,16 +8,20 @@ import Footer from "./styledComponents/Footer.style";
 import Home from "./styledComponents/Home.style";
 import Carousel from "./styledComponents/Carousel.style";
 
+export const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <Header className="header" />
-    <Carousel className="Carousel" />
-    <Home className="Home" />
-    <Footer className="Footer" />
-    <GlobalStyle />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient} contextSharing={true}>
+    <React.StrictMode>
+      <Header className="header" />
+      <Carousel className="Carousel" />
+      <Home className="Home" />
+      <Footer className="Footer" />
+      <GlobalStyle />
+    </React.StrictMode>
+  </QueryClientProvider>
 );

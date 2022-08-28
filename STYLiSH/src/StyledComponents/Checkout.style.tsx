2185ec32c4bf132eaaa2150625e321a111e4f92f
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-28 19:50:18 +0800

    Delete STYLiSH/src/StyledComponents directory

--- STYLiSH/src/StyledComponents/Checkout.style.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import mediaQuery from "./mediaQuery";
-import Checkout from "../Checkout";
-
-const CheckoutStyledComponents = styled(Checkout)``;
-
-export default CheckoutStyledComponents;
--- STYLiSH/src/StyledComponents/Product.style.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import mediaQuery from "./mediaQuery";
-import Product from "../Product";
-
-const ProductStyledComponents = styled(Product)``;
-
-export default ProductStyledComponents;

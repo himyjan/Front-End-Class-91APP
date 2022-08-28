a7556b92a0b0e44be621dc2964dc1b93b315eeda
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-28 19:51:42 +0800

    day25 w2p5 fix dir

---
+++ STYLiSH/src/styledComponents/Checkout.style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import mediaQuery from "./mediaQuery";
+import Checkout from "../Checkout";
+
+const CheckoutStyledComponents = styled(Checkout)``;
+
+export default CheckoutStyledComponents;

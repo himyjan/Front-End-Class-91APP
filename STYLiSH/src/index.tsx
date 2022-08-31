3471ded74f4823f3553cd182c24ab45397ff0846
himyjan <51815522+himyjan@users.noreply.github.com>
2022-08-31 13:23:29 +0800

    day27 1.w2p5 home page category with url phase2 2.w3p3 checkout page react hook form

--- STYLiSH/src/index.tsx
+++ STYLiSH/src/index.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, ReactLocation, Router } from "@tanstack/react-location";
 
 import GlobalStyle from "./styledComponents/index.style";
 import Header from "./styledComponents/Header.style";
 import Footer from "./styledComponents/Footer.style";
-import Home from "./styledComponents/Home.style";
 import Carousel from "./styledComponents/Carousel.style";
+import Home from "./styledComponents/Home.style";
+import Product from "./styledComponents/Product.style";
+import Checkout from "./styledComponents/Checkout.style";
+import {
+  getCategoryAllDataLoader,
+  getCategoryWomenDataLoader,
+  getCategoryMenDataLoader,
+  getCategoryAccessoriesDataLoader,
+  getProductDataLoader,
+  ApiDataJson,
+} from "./lib/fetchAPI";
 
 export const queryClient = new QueryClient();
+export const location = new ReactLocation();
+
+export const getQueryClientFetchData = (key: string[]) => {
+  return queryClient.getQueryData(key) as ApiDataJson;
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,12 +32,122 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <QueryClientProvider client={queryClient} contextSharing={true}>
-    <React.StrictMode>
-      <Header className="header" />
-      <Carousel className="Carousel" />
-      <Home className="Home" />
-      <Footer className="Footer" />
-      <GlobalStyle />
-    </React.StrictMode>
+    <Router
+      location={location}
+      routes={[
+        {
+          id: "women",
+          path: "/",
+          search: (search) => {
+            return search.category === "women";
+          },
+          loader: () =>
+            queryClient.getQueryData(["WomenData"]) ??
+            queryClient
+              .fetchQuery(["WomenData"], getCategoryWomenDataLoader)
+              .then((res) => res.data),
+          element: (
+            <>
+              <Carousel className="Carousel" />
+              <Home className="Home" />
+            </>
+          ),
+        },
+        {
+          id: "men",
+          path: "/",
+          search: (search) => {
+            return search.category === "men";
+          },
+          loader: () =>
+            queryClient.getQueryData(["MenData"]) ??
+            queryClient
+              .fetchQuery(["MenData"], getCategoryMenDataLoader)
+              .then((res) => res.data),
+          element: (
+            <>
+              <Carousel className="Carousel" />
+              <Home className="Home" />
+            </>
+          ),
+        },
+        {
+          id: "accessories",
+          path: "/",
+          search: (search) => {
+            return search.category === "accessories";
+          },
+          loader: () =>
+            queryClient.getQueryData(["AccessoriesData"]) ??
+            queryClient
+              .fetchQuery(["AccessoriesData"], getCategoryAccessoriesDataLoader)
+              .then((res) => res.data),
+          element: (
+            <>
+              <Carousel className="Carousel" />
+              <Home className="Home" />
+            </>
+          ),
+        },
+        {
+          id: "search",
+          path: "/",
+          search: (search) => {
+            return (
+              typeof search.keyword === "string" ||
+              search.keyword instanceof String
+            );
+          },
+        },
+        {
+          id: "all",
+          path: "/",
+          loader: () =>
+            queryClient.getQueryData(["AllData"]) ??
+            queryClient.fetchQuery(["AllData"], getCategoryAllDataLoader),
+          element: (
+            <>
+              <Carousel className="Carousel" />
+              <Home className="Home" />
+            </>
+          ),
+        },
+        {
+          id: "product",
+          path: "product",
+          children: [
+            {
+              path: ":product_id",
+              loader: ({ params: { product_id } }) =>
+                queryClient.getQueryData(["product", product_id]) ??
+                queryClient.fetchQuery(["product", product_id], () =>
+                  getProductDataLoader(product_id)
+                ),
+              element: (
+                <>
+                  <Product className="Product" />
+                </>
+              ),
+            },
+          ],
+        },
+        {
+          id: "checkout",
+          path: "checkout",
+          element: (
+            <>
+              <Checkout className="Checkout" />
+            </>
+          ),
+        },
+      ]}
+    >
+      <React.StrictMode>
+        <Header className="header" />
+        <Outlet />
+        <Footer className="Footer" />
+        <GlobalStyle />
+      </React.StrictMode>
+    </Router>
   </QueryClientProvider>
 );

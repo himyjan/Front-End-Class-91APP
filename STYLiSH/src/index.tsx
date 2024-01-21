7776af76a8c5a6b756dbfdbdefc230d51523a8eb
himyjan <51815522+himyjan@users.noreply.github.com>
2024-01-21 20:22:55 +0800

    update version

--- STYLiSH/src/index.tsx
+++ STYLiSH/src/index.tsx
@@ -162,7 +162,7 @@ const TanStackRouterDevtools =
     ? () => null // Render nothing in production
     : React.lazy(() =>
       // Lazy load in development
-      import('@tanstack/react-router-devtools').then((res) => ({
+      import('@tanstack/router-devtools').then((res) => ({
         default: res.TanStackRouterDevtools,
         // For Embedded Mode
         // default: res.TanStackRouterDevtoolsPanel
